490ce912de0f9108ee338b33c872cf10caff5594
ntanh23112005 <anhntps38560@gmail.com>
2025-03-26 22:42:46 +0700

    this is react stage 1

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Hello World Thế Anh</h1>
+      <h2>This is React Stage 1 </h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
